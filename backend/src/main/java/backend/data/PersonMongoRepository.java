c44f3fabf771aab9327cfaf024c7978b1e1f2de6
Juan Soria <soria.juan@gmail.com>
2022-01-23 09:48:14 -0300

    added querydsl support

--- backend/src/main/java/backend/data/PersonMongoRepository.java
+++ backend/src/main/java/backend/data/PersonMongoRepository.java
@@ -1,6 +1,7 @@
 package backend.data;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-interface PersonMongoRepository extends MongoRepository<PersonMongo, String> {
+interface PersonMongoRepository extends MongoRepository<PersonMongo, String>, QuerydslPredicateExecutor<PersonMongo> {
 }
