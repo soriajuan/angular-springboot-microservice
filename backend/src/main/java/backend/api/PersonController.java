2f8d9450adee5cef4dff290ceb22fa7dedfbd6e4
Juan Soria <soria.juan@gmail.com>
2023-05-29 20:12:41 -0300

    Fixed backend tests

--- backend/src/main/java/backend/api/PersonController.java
+++ backend/src/main/java/backend/api/PersonController.java
@@ -1,9 +1,14 @@
 package backend.api;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import backend.api.payload.PersonGetResponsePayload;
+import backend.api.payload.PersonPatchRequestPayload;
+import backend.api.payload.PersonPostRequestPayload;
+import backend.domain.entity.Person;
+import backend.domain.usecase.CreatePersonUseCase;
+import backend.domain.usecase.DeletePersonUseCase;
+import backend.domain.usecase.ReadPersonUseCase;
+import backend.domain.usecase.UpdatePersonUseCase;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,55 +23,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import backend.api.payload.PersonGetResponsePayload;
-import backend.api.payload.PersonPatchRequestPayload;
-import backend.api.payload.PersonPostRequestPayload;
-import backend.domain.entity.Person;
-import backend.domain.usecase.CreatePersonUseCase;
-import backend.domain.usecase.DeletePersonUseCase;
-import backend.domain.usecase.ReadPersonUseCase;
-import backend.domain.usecase.UpdatePersonUseCase;
-import lombok.RequiredArgsConstructor;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/persons")
 @RequiredArgsConstructor
-public class PersonController {
+class PersonController {
 
-	private final CreatePersonUseCase createUseCase;
-	private final ReadPersonUseCase readUseCase;
-	private final UpdatePersonUseCase updateUseCase;
-	private final DeletePersonUseCase deleteUseCase;
-	private final ApiMapper mapper;
+    private final CreatePersonUseCase createUseCase;
+    private final ReadPersonUseCase readUseCase;
+    private final UpdatePersonUseCase updateUseCase;
+    private final DeletePersonUseCase deleteUseCase;
+    private final ApiMapper mapper;
 
-	@GetMapping
-	public List<PersonGetResponsePayload> getAll() {
-		return readUseCase.readAll().stream().map(d -> mapper.toPersonGetResponsePayload(d))
-				.collect(Collectors.toList());
-	}
+    @GetMapping
+    List<PersonGetResponsePayload> getAll() {
+        return readUseCase.readAll().stream().map(mapper::toPersonGetResponsePayload)
+                .collect(Collectors.toList());
+    }
 
-	@GetMapping(value = "/{id}")
-	public PersonGetResponsePayload getById(@PathVariable(value = "id") String id) {
-		return mapper.toPersonGetResponsePayload(readUseCase.readById(id));
-	}
+    @GetMapping(value = "/{id}")
+    PersonGetResponsePayload getById(@PathVariable(value = "id") String id) {
+        return mapper.toPersonGetResponsePayload(readUseCase.readById(id));
+    }
 
-	@PostMapping
-	@CrossOrigin(exposedHeaders = { HttpHeaders.LOCATION })
-	public ResponseEntity<Void> post(@RequestBody PersonPostRequestPayload payload) {
-		Person person = createUseCase.create(mapper.toPerson(payload));
-		return ResponseEntity.created(URI.create("/persons/" + person.getId())).build();
-	}
+    @PostMapping
+    @CrossOrigin(exposedHeaders = {HttpHeaders.LOCATION})
+    ResponseEntity<Void> post(@RequestBody PersonPostRequestPayload payload) {
+        Person person = createUseCase.create(mapper.toPerson(payload));
+        return ResponseEntity.created(URI.create("/persons/" + person.getId())).build();
+    }
 
-	@PatchMapping(value = "/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void patch(@PathVariable(value = "id") String id, @RequestBody PersonPatchRequestPayload payload) {
-		updateUseCase.update(id, mapper.toPerson(payload));
-	}
+    @PatchMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void patch(@PathVariable(value = "id") String id, @RequestBody PersonPatchRequestPayload payload) {
+        updateUseCase.update(id, mapper.toPerson(payload));
+    }
 
-	@DeleteMapping(value = "/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@PathVariable(value = "id") String id) {
-		deleteUseCase.deleteById(id);
-	}
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void delete(@PathVariable(value = "id") String id) {
+        deleteUseCase.deleteById(id);
+    }
 
 }
--- backend/src/test/java/backend/api/PersonControllerTest.java
+++ backend/src/test/java/backend/api/PersonControllerTest.java
@@ -1,137 +1,202 @@
 package backend.api;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.time.LocalDate;
-import java.util.List;
-
-import org.approvaltests.Approvals;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
+import backend.api.payload.PersonGetResponsePayload;
+import backend.api.payload.PersonPatchRequestPayload;
+import backend.api.payload.PersonPostRequestPayload;
 import backend.domain.entity.Person;
 import backend.domain.usecase.CreatePersonUseCase;
 import backend.domain.usecase.DeletePersonUseCase;
 import backend.domain.usecase.ReadPersonUseCase;
 import backend.domain.usecase.UpdatePersonUseCase;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-@ExtendWith(SpringExtension.class)
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(PersonController.class)
-public class PersonControllerTest {
+class PersonControllerTest {
 
-	@Autowired
-	MockMvc mockMvc;
+    static final Person NIKOLA_TESLA = Person.builder().id("1").firstName("Nikola").lastName("Tesla").dateOfBirth(LocalDate.of(1856, 7, 10))
+            .build();
 
-	@MockBean
-	CreatePersonUseCase createUseCase;
+    @Autowired
+    MockMvc mockMvc;
 
-	@MockBean
-	ReadPersonUseCase readUseCase;
+    @MockBean
+    CreatePersonUseCase createUseCase;
 
-	@MockBean
-	UpdatePersonUseCase updateUseCase;
+    @MockBean
+    ReadPersonUseCase readUseCase;
 
-	@MockBean
-	DeletePersonUseCase deleteUseCase;
+    @MockBean
+    UpdatePersonUseCase updateUseCase;
 
-	@MockBean
-	ApiMapper mapper;
+    @MockBean
+    DeletePersonUseCase deleteUseCase;
 
-	@Test @Disabled
-	public void getAllOk() throws Exception {
-		List<Person> personsMock = List.of(
-				Person.builder().id("1").firstName("Nikola").lastName("Tesla").dateOfBirth(LocalDate.of(1856, 7, 10))
-						.build(),
-				Person.builder().id("2").firstName("Albert").lastName("Einstein").dateOfBirth(LocalDate.of(1879, 3, 14))
-						.build(),
-				Person.builder().id("3").firstName("Carl").lastName("Sagan").dateOfBirth(LocalDate.of(1934, 11, 9))
-						.build());
-		when(readUseCase.readAll()).thenReturn(personsMock);
-		//when(mapper.toPersonGetResponsePayload(any(Person.class))).thenReturn(any(PersonGetResponsePayload.class));
+    @MockBean
+    ApiMapper mapper;
 
-		MvcResult mvcResult = mockMvc.perform(get("/persons")).andExpect(status().isOk()).andReturn();
+    @Test
+    void getAllPersons() throws Exception {
+        List<Person> personsMock = List.of(
+                NIKOLA_TESLA,
+                Person.builder().id("2").firstName("Albert").lastName("Einstein").dateOfBirth(LocalDate.of(1879, 3, 14))
+                        .build(),
+                Person.builder().id("3").firstName("Carl").lastName("Sagan").dateOfBirth(LocalDate.of(1934, 11, 9))
+                        .build());
+        when(readUseCase.readAll()).thenReturn(personsMock);
+        doAnswer(i -> {
+            var p = i.getArgument(0, Person.class);
+            var r = new PersonGetResponsePayload();
+            r.setId(p.getId());
+            r.setFirstName(p.getFirstName());
+            r.setLastName(p.getLastName());
+            r.setDateOfBirth(p.getDateOfBirth());
+            return r;
+        }).when(mapper).toPersonGetResponsePayload(any(Person.class));
 
-		Approvals.verify(mvcResult.getResponse().getContentAsString());
+        MvcResult mvcResult = mockMvc.perform(get("/persons"))
+                .andExpect(status().isOk())
+                .andReturn();
 
-		verify(readUseCase).readAll();
-		verifyNoMoreInteractions(readUseCase);
-		// verifyNoInteractions(mapper);
-	}
+        Approvals.verify(mvcResult.getResponse().getContentAsString());
 
-//	@Test
-//	public void getByIdOk() throws Exception {
-//		Long id = 1L;
-//		String string = "string";
-//		Integer ano = 2021;
-//		Boolean vendido = false;
-//		LocalDateTime now = LocalDateTime.of(2021, 9, 18, 0, 0);
-//
-////		Person veiculo = Person.builder().id(id).veiculo(string).marca(string).ano(ano).descricao(string)
-////				.vendido(vendido).created(now).updated(now).build();
-//		Person veiculo = null;
-//		when(readUseCase.readById(anyLong())).thenReturn(veiculo);
-//
-//		PersonGetResponsePayload veiculoGetResponsePayload = new PersonGetResponsePayload();
-//		veiculoGetResponsePayload.setId(id);
-////		veiculoGetResponsePayload.setVeiculo(string);
-////		veiculoGetResponsePayload.setMarca(string);
-////		veiculoGetResponsePayload.setAno(ano);
-////		veiculoGetResponsePayload.setDescricao(string);
-////		veiculoGetResponsePayload.setVendido(vendido);
-////		veiculoGetResponsePayload.setCreated(now);
-////		veiculoGetResponsePayload.setUpdated(now);
-////		when(mapper.toVeiculoGetResponsePayload(any(Person.class))).thenReturn(veiculoGetResponsePayload);
-//
-//		MvcResult mvcResult = this.mockMvc.perform(get("/veiculos/{id}", 1)).andExpect(status().isOk()).andReturn();
-//
-//		Approvals.verifyJson(mvcResult.getResponse().getContentAsString());
-//	}
-//
-//	@Test
-//	public void postCreated() throws Exception {
-//		Long id = 1L;
-//		String string = "string";
-//		Integer ano = 2021;
-//		Boolean vendido = false;
-//
-////		Person veiculo = Person.builder().veiculo(string).marca(string).ano(ano).descricao(string).vendido(vendido)
-////				.build();
-//		Person veiculo = null;
-////		when(mapper.toVeiculo(any(PersonPostRequestPayload.class))).thenReturn(veiculo);
-//
-//		when(createUseCase.create(any(Person.class)))
-//				.thenReturn(veiculo);
-//
-//		PersonPostRequestPayload veiculoPostRequestPayload = new PersonPostRequestPayload();
-////		veiculoPostRequestPayload.setVeiculo(string);
-////		veiculoPostRequestPayload.setMarca(string);
-////		veiculoPostRequestPayload.setAno(ano);
-////		veiculoPostRequestPayload.setDescricao(string);
-////		veiculoPostRequestPayload.setVendido(vendido);
-//
-//		this.mockMvc
-//				.perform(post("/veiculos").contentType(MediaType.APPLICATION_JSON)
-//						.content(new ObjectMapper().writeValueAsString(veiculoPostRequestPayload)))
-//				.andDo(print()).andExpect(status().isCreated())
-//				.andExpect(header().string("Location", "/veiculos/" + id));
-//	}
-//
-//	@Test
-//	public void deleteNoContent() throws Exception {
-//		doNothing().when(deleteUseCase).deleteById(anyLong());
-//
-//		this.mockMvc.perform(delete("/veiculos/{id}", 1)).andDo(print()).andExpect(status().isNoContent());
-//	}
+        verify(readUseCase).readAll();
+        verify(mapper, times(3)).toPersonGetResponsePayload(any(Person.class));
+        verifyNoMoreInteractions(readUseCase, mapper);
+    }
+
+    @Test
+    void getPersonById() throws Exception {
+        var id = "1";
+
+        when(readUseCase.readById(id))
+                .thenReturn(NIKOLA_TESLA);
+        doAnswer(i -> {
+            var p = i.getArgument(0, Person.class);
+            var r = new PersonGetResponsePayload();
+            r.setId(p.getId());
+            r.setFirstName(p.getFirstName());
+            r.setLastName(p.getLastName());
+            r.setDateOfBirth(p.getDateOfBirth());
+            return r;
+        }).when(mapper).toPersonGetResponsePayload(any(Person.class));
+
+        MvcResult mvcResult = mockMvc.perform(get("/persons/{id}", id))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Approvals.verifyJson(mvcResult.getResponse().getContentAsString());
+
+        verify(readUseCase).readById(id);
+        verify(mapper).toPersonGetResponsePayload(any(Person.class));
+        verifyNoMoreInteractions(readUseCase, mapper);
+    }
+
+    @Test
+    void createPerson() throws Exception {
+        var personPostRequestPayload = new PersonPostRequestPayload();
+        personPostRequestPayload.setFirstName(NIKOLA_TESLA.getFirstName());
+        personPostRequestPayload.setLastName(NIKOLA_TESLA.getLastName());
+        personPostRequestPayload.setDateOfBirth(NIKOLA_TESLA.getDateOfBirth());
+
+        var person = Person.builder()
+                .firstName(NIKOLA_TESLA.getFirstName())
+                .lastName(NIKOLA_TESLA.getLastName())
+                .dateOfBirth(NIKOLA_TESLA.getDateOfBirth())
+                .build();
+
+        doAnswer(i -> person)
+                .when(mapper).toPerson(personPostRequestPayload);
+
+        doReturn(NIKOLA_TESLA)
+                .when(createUseCase).create(person);
+
+        var objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        mockMvc
+                .perform(post("/persons")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(personPostRequestPayload)))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/persons/1"));
+
+        verify(mapper).toPerson(personPostRequestPayload);
+        verify(createUseCase).create(person);
+        verifyNoMoreInteractions(mapper, readUseCase);
+    }
+
+    @Test
+    void updatePerson() throws Exception {
+        var id = "1";
+
+        var personPatchRequestPayload = new PersonPatchRequestPayload();
+        personPatchRequestPayload.setFirstName("NIKOLA");
+        personPatchRequestPayload.setLastName("TESLA");
+
+        var person = Person.builder()
+                .firstName("NIKOLA")
+                .lastName("TESLA")
+                .build();
+
+        doAnswer(i -> person)
+                .when(mapper).toPerson(personPatchRequestPayload);
+
+        doReturn(person.toBuilder()
+                .id(id)
+                .dateOfBirth(NIKOLA_TESLA.getDateOfBirth())
+                .build())
+                .when(updateUseCase).update(id, person);
+
+        var objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        mockMvc
+                .perform(patch("/persons/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(personPatchRequestPayload)))
+                .andExpect(status().isNoContent());
+
+        verify(mapper).toPerson(personPatchRequestPayload);
+        verify(updateUseCase).update(id, person);
+        verifyNoMoreInteractions(mapper, updateUseCase);
+    }
+
+    @Test
+    void deletePerson() throws Exception {
+        var id = "1";
+
+        doNothing()
+                .when(deleteUseCase).deleteById(id);
+
+        mockMvc.perform(delete("/persons/{id}", id))
+                .andExpect(status().isNoContent());
+    }
 
 }
